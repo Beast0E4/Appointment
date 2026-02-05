596c77d02e89b9c86d4a53e334fe59d9328abcdc
Ebison Sojan <ebison.04@gmail.com>
2026-02-05 18:13:19 +0530

    Fix: Removed provider slice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,7 +3,6 @@ import authReducer from './slices/auth.slice';
 import serviceReducer from './slices/service.slice';
 import appointmentReducer from './slices/appointment.slice';
 import availabilityReducer from './slices/availability.slice';
-import providerReducer from './slices/provider.slice';
 
 export const store = configureStore({
   reducer: {
@@ -11,7 +10,6 @@ export const store = configureStore({
     services: serviceReducer,
     appointments: appointmentReducer,
     availability: availabilityReducer,
-    provider: providerReducer,
   },
 });
 
